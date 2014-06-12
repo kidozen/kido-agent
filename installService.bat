2bc16e421545ff6d369b7a2d828996ac16834330
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2014-06-12 15:16:31 -0300

    fixed installService.bat syntax

--- installService.bat
+++ installService.bat
@@ -1,9 +1,20 @@
 @ECHO OFF
 
+
 SET /p nodePath=Enter Node.js installation path (C:\Program Files\nodejs\node.exe):
+
+
 for /f "delims=" %%a in ('npm config get prefix') do @set npmPrefix=%%a
-SET "kidoAgentPath=%npmPrefix%\node_modules\kido-agent\bin\server"
+
+
+SET kidoAgentPath=%npmPrefix%\node_modules\kido-agent\bin\server
+
 IF %nodePath% == "" (SET nodePath="C:\Program Files\nodejs\node.exe")
+
 IF EXIST "%PROGRAMFILES(X86)%" (SET nssmPath="nssm64.exe") ELSE (SET nssmPath="nssm32.exe")
+
 %npmPrefix%\node_modules\kido-agent\bin\%nssmPath% install kido-agent %nodePath% %kidoAgentPath%
+
+
+
 pause
